2e59d446c0dfe2d8ba7a4d7aadcd230f528f3b3d
Stanley Smith <swsmth@berkeley.edu>
2017-02-20 13:01:46 -0800

    updated platoon_v1

--- platoon_v1.m
+++ platoon_v1.m
@@ -3,46 +3,42 @@
 % (For one lead vehicle, two following vehicles)
 % (all units in meters, meters/second)
 %
-%      [xld]    [0  1  0  0  0  0][xld]   [      0      ]   [  0  ]
-%   d  [vld]    [0  0  0  0  0  0][vld]   [    uld(t)   ]   [  0  ]
-%   -- [x1 ] =  [0  0  1  0  0  0][x1 ] + [      0      ] + [  0  ]   (1)
-%   dt [v1 ]    [0  0 -1  0  0  0][v1 ]   [  u1(xld,x1) ]   [nv_1 ]
-%      [x2 ]    [0  0  0  1  0  0][x2 ]   [      0      ]   [  0  ]
-%      [v2 ]    [0  0  0 -1  0  0][v2 ]   [  u2(x1,x2)  ]   [nv_2 ]
+%      [xL]    [0  1  0  0  0  0][xld]   [      0      ]   [  0  ]
+%   d  [vL]    [0  0  0  0  0  0][vld]   [    uL(t)   ]   [  0  ]
+%   -- [x1] =  [0  0  1  0  0  0][x1 ] + [      0      ] + [  0  ]   (1)
+%   dt [v1]    [0  0 -1  0  0  0][v1 ]   [  u1(xld,x1) ]   [ v01 ]
+%      [x2]    [0  0  0  1  0  0][x2 ]   [      0      ]   [  0  ]
+%      [v2]    [0  0  0 -1  0  0][v2 ]   [  u2(x1,x2)  ]   [ v02 ]
 %
 % Variables:
 % ----------
-% * xld = lead vehicle position
-% * vld = lead vehicle velocity
+% * xL = lead vehicle position
+% * vL = lead vehicle velocity
 % * x1 = follower vehicle 1 position
 % * v1 = follower vehicle 1 velocity
 % * x2 = follower vehicle 2 position
 % * v2 = follower vehicle 2 velocity
-% * uld(t) = input chosen externally (to accomplish some LTL spec, etc. )
+% * uL(t) = input chosen externally (to accomplish some LTL spec, etc. )
 % * u1(xld,x1) = unidirectional spring pushing back follower 1, with the
 %   goal of centering it around a desired headway
 % * u2(x1,x2) = same for follower 2
-% * nv_l = lead vehicle nominal velocity
-% * nv_f1 = follower vehicle 1 nominal velocity
-% * nv_f2 = follower vehicle 2 nominal velocity
+% * v01 = follower vehicle 1 nominal velocity
+% * v02 = follower vehicle 2 nominal velocity
 
 % Platoon 1
 
 % unidirectional virtual springs
+% using, e.g., u2(x1,x2) = k(x1 - x2 - d2)
+
 d1 = 50; % desired following distance
 d2 = 50;
 k1 = 1; % "spring constant" or gain
 k2 = 1;
 
-spring_f1 = k1.*[1 0 -1 0 0 0];
-spring_f2 = k2.*[0 0 1 0 -1 0];
-
 % nominal velocities
-nv_l = 25;
-nv_f1 = 25;
-nv_f2 = 25; % can modify, with effects to following distances
+v01 = 25;
+v02 = 25; % can modify, with effects to following distances
 
-% go without nominal velocity
 A_sub = [0 1 0 0 0 0;
         0 0 0 0 0 0;
         0 0 0 1 0 0;
@@ -52,16 +48,16 @@ A_sub = [0 1 0 0 0 0;
 
 B_sub = [0; 1; 0; 0; 0; 0];
 
-K_sub = [0; nv_l; 0; nv_f1 - k1*d1; 0; nv_f2 - k2*d2];
+K_sub = [0; 0; 0; v01 - k1*d1; 0; v02 - k2*d2];
 
 % Platoon 2 - identical
 
 % Combined system
 % Add - additional spring between platoons
 
-A_c = blkdiag(A_sub, A_sub);
-B_C = blkdiag(B_sun, B_sub);
-K_c = [K_sub; K_sub];
+A = blkdiag(A_sub, A_sub);
+B = blkdiag(B_sub, B_sub);
+K = [K_sub; K_sub];
 
 d_platoon = 50;
 k_platoon = 1;
@@ -69,25 +65,55 @@ k_platoon = 1;
 spring_platoon = ...
         k_platoon.*[0 0 0 0 1 0 -1 0 0 0 0 0];
     
-A_c(8,:) = A_c(8,:) + spring_platoon;
-K_c(8,:) = K_c(8,:) - d_platoon*k_platoon;
+A(8,:) = A(8,:) + spring_platoon;
+K(8,:) = K(8,:) - k_platoon*d_platoon;
 
-% Define a transformation to the following 4 states:
+% Define a transformation for following 4 states:
 
-% x_l1 = lead platoon position
-% v_l1 = lead platoon velocity
-% x_l2 = following platoon position
-% v_l2 = following platoon velocity
+% x = Pz + omega
+
+% Where z(1 - 4) are the following:
+% z1 = xL1 = lead platoon position
+% z2 = vL1 = lead platoon velocity
+% z3 = xL2 = following platoon position
+% z4 = vL2 = following platoon velocity
+
+P_sub = [1 0;
+    0 1;
+    1, -1/k1;
+    0 1;
+    1, -1/k1-1/k2;
+    0 1];
+
+P = blkdiag(P_sub, P_sub);
+
+omega_sub = [0; 0; -d1 + v01/k1; 0; -d1 - d2 + v01/k1 + v02/k2; 0];
+
+omega = [omega_sub;
+         omega_sub];
 
 % Abstraction system
 
-F = P_sword*A_c*P;
-G = P_sword*B_c;
-L = P_sword*K_c;
-error = P*F - A_c*P;
+F_sub = [0 1;
+         0 0];
+     
+F = blkdiag(F_sub, F_sub);
+     
+G_sub = [0;
+         1];
+     
+G = blkdiag(G_sub, G_sub);
+    
+L_sub = [0;
+         0];
+     
+L = [L_sub;
+     L_sub];
+     
+error = P*F - A*P;
 
 % use Q to cancel out error as much as possible
-Q = B_c\error;
+Q = B\error;
 
 % discretize w/ dt = 0.1s
 
@@ -97,11 +123,11 @@ F_d = F_s(dt);
 G_d = integral(F_s, 0, dt, 'ArrayValued', true) * G;
 L_d = integral(F_s, 0, dt, 'ArrayValued', true) * L;
 
-% define safe set to be 2s < time-headway < 3.5s
-time_headway_lo = 2;
-time_headway_hi = 3.5;
+% define safe set to be 6s < time-headway < 8s
+th_lo = 6;
+th_hi = 38;
 
-% keep lead/following vehicle in between 22 and 28 m/s
+% keep lead/following vehicle in between 20 and 30 m/s
 pl_lo = 20;
 pl_hi = 30;
 pf_lo = 20;
@@ -137,19 +163,17 @@ C = ls.ConInvOI(Z);
 
 % save model parameters for Simulink %
 
-% 3-dimensional abstraction - continuous
-model.F = F;
-model.G = G;
-model.L = L;
-
-% 3-dimensional abstraction - discrete
-model.F_d = F_d;
-model.G_d = G_d;
-model.L_d = L_d;
-
-% 11-dimensional model - continuous
-model.A = A_c;
-model.B = B_c;
-model.K = K_c;
-
+% % 3-dimensional abstraction - continuous
+% model.F = F;
+% model.G = G;
+% model.L = L;
 % 
+% % 3-dimensional abstraction - discrete
+% model.F_d = F_d;
+% model.G_d = G_d;
+% model.L_d = L_d;
+% 
+% % 11-dimensional model - continuous
+% model.A = A_c;
+% model.B = B_c;
+% model.K = K_c;
