225652e36c39cbe0a98e4812ada83a98e6dd533d
Stanley Smith <swsmth@berkeley.edu>
2017-02-27 11:59:01 -0800

    completed simulation & plotting

--- platoon.m
+++ platoon.m
@@ -14,11 +14,11 @@ z0 = [300; 0; 150; 0]; % initial conditions
 parameters = set_parameters; % set parameters for platoon
 
 % simulate concrete platoon
-[tx,x] = ode45(@(t1,x) NLPlatoonFull(t1,x,parameters), [0, 120], x0);
+[tx,x] = ode45(@(t1,x) NLPlatoonFull(t1,x,parameters), [0, 80], x0);
 x = x';
 
 % simulate abstraction platoon
-[tz,z] = ode45(@(t2,z) NLPlatoonAbs(t2,z,parameters), [0, 120], z0);
+[tz,z] = ode45(@(t2,z) NLPlatoonAbs(t2,z,parameters), [0, 80], z0);
 z = z';
 
 % get e(t) = norm(x(t) - (Pz(t) + omega))
@@ -29,18 +29,36 @@ input2 = zeros(size(tx));
 
 % post-compute inputs (fix this later)
 for i = 1:length(tx)
-    input2(i) = u2(tx(i),x(5,i) - x(7,i), parameters);
+    input_u1(i) = u1(tx(i));
+    input_u2(i) = u2(tx(i),x(5,i) - x(7,i), parameters);
+    input_v1(i) = v1(tx(i));
+    input_v2(i) = v2(tx(i));
 end
+
+position_indices = [1; 3; 5; 7; 9; 11];
 
 % plot results
 subplot(2,2,1);
-plot(tx,x);
+plot(tx,x(position_indices,:));
+title('x(t)');
+xlabel('Time (s)');
+ylabel('Distance (m)');
 
 subplot(2,2,2);
-plot(tz,xl);
+plot(tz,xl(position_indices,:));
+title('Pz(t) - \Omega');
+xlabel('Time (s)');
+ylabel('Distance (m)');
 
 subplot(2,2,3);
-plot(tx,input2,tx,u1(tx));
+plot(tx,input_u2,tx,input_v2);
+title('u_2(t) & v_2(t)');
+xlabel('Time (s)');
+ylabel('Acceleration (m/s^2)');
+legend('v_2(t)','u_2(t)');
 
 subplot(2,2,4);
 plot(tx,e);
+title('|x(t) - Pz(t) + \Omega|_2');
+xlabel('Time (s)');
+ylabel('State Space Distance');
