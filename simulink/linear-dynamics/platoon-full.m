5e8ebeb53b3271de7ef09098e11d63685ad423c4
Stanley Smith <swsmth@berkeley.edu>
2017-03-04 15:44:55 -0800

    added simulink folder

---
+++ simulink/linear-dynamics/platoon-full.m
@@ -0,0 +1,65 @@
+function msfcn_limintm(block)
+
+  setup(block);
+  
+%endfunction
+
+function setup(block)
+  
+  %% Register number of dialog parameters   
+  block.NumDialogPrms = 0;
+
+  %% Register number of input and output ports
+  block.NumInputPorts  = 1;
+  block.NumOutputPorts = 1;
+
+  %% Setup functional port properties to dynamically
+  %% inherited.
+  block.SetPreCompInpPortInfoToDynamic;
+  block.SetPreCompOutPortInfoToDynamic;
+ 
+  block.InputPort(1).Dimensions        = 2;
+  block.InputPort(1).DirectFeedthrough = false;
+  
+  block.OutputPort(1).Dimensions       = 12;
+  
+  %% Set block sample time to continuous
+  block.SampleTimes = [0 0];
+  
+  %% Setup Dwork
+  block.NumContStates = 12;
+
+  %% Set the block simStateCompliance to default (i.e., same as a built-in block)
+  block.SimStateCompliance = 'DefaultSimState';
+
+  %% Register methods
+  block.RegBlockMethod('InitializeConditions',    @InitConditions);  
+  block.RegBlockMethod('Outputs',                 @Output);  
+  block.RegBlockMethod('Derivatives',             @Derivative);  
+  
+%endfunction
+
+function InitConditions(block)
+
+  %% Initialize Dwork
+  block.ContStates.Data = % insert initial state;
+  
+%endfunction
+
+function Output(block)
+
+  block.OutputPort(1).Data = block.ContStates.Data;
+  
+%endfunction
+
+function Derivative(block)
+    global parameters;
+
+    % full platoon dynamics (12 states)
+    
+    x = block.InputPort(1).Data;
+    u = block.InputPort(2).Data;
+    
+    block.Derivatives.Data(1) = 
+  
+%endfunction
