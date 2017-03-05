5404b781ebd20539478b5f2459ac8718393c9ae9
Stanley Smith <swsmth@berkeley.edu>
2017-03-04 21:34:37 -0800

    add code to try and find stabilizing feedback

--- simulink/linear-dynamics/run_setup.m
+++ simulink/linear-dynamics/run_setup.m
@@ -1,42 +1,42 @@
-function par = set_parameters
-% set all necessary parameters for platoon simulation
+function mdl = run_setup
+% mdl = struct of necessary model parameters for simulation
 
 % nominal velocities
-par.v0L1 = 25;
-par.v0L2 = 25;
-par.v01 = 25;
-par.v02 = 25;
-par.v03 = 25;
-par.v04 = 25; % each vehicle has the same nominal velocity
-par.dh = 50; % desired headway
-par.k = 5; % gain for spring
+mdl.v0L1 = 25;
+mdl.v0L2 = 25;
+mdl.v01 = 25;
+mdl.v02 = 25;
+mdl.v03 = 25;
+mdl.v04 = 25; % each vehicle has the same nominal velocity
+mdl.dh = 50; % desired headway
+mdl.k = 5; % gain for spring
 
 % concrete system dynamics %
 A_sub = [0 1 0 0 0 0;
         0 -1 0 0 0 0;
         0 0 0 1 0 0;
-        par.k 0 -par.k -1 0 0;
+        mdl.k 0 -mdl.k -1 0 0;
         0 0 0 0 0 1;
-        0 0 par.k 0 -par.k -1]; % dynamics for one platoon subsystem
+        0 0 mdl.k 0 -mdl.k -1]; % dynamics for one platoon subsystem
 A = blkdiag(A_sub, A_sub);
-A(8,:) = A(8,:) + [0 0 0 0 par.k 0 -par.k 0 0 0 0 0]; % add spring between platoons
-par.A = A;
+A(8,:) = A(8,:) + [0 0 0 0 mdl.k 0 -mdl.k 0 0 0 0 0]; % add spring between platoons
+mdl.A = A;
 B_sub = [0; 1; 0; 0; 0; 0];
-par.B = blkdiag(B_sub, B_sub); % control input
-par.theta = [0; par.v0L1; 0; par.v01 - par.k*par.dh; 0; par.v02 - par.k*par.dh; ...
-            0; par.v0L2 - par.k*par.dh; 0; par.v03 - par.k*par.dh; 0; par.v04 - par.k*par.dh];
+mdl.B = blkdiag(B_sub, B_sub); % control input
+mdl.theta = [0; mdl.v0L1; 0; mdl.v01 - mdl.k*mdl.dh; 0; mdl.v02 - mdl.k*mdl.dh; ...
+          0; mdl.v0L2 - mdl.k*mdl.dh; 0; mdl.v03 - mdl.k*mdl.dh; 0; mdl.v04 - mdl.k*mdl.dh];
         % constant dynamics
 
 % reference system dynamics %
-par.F = [0 1 0 0;
+mdl.F = [0 1 0 0;
     0 -1 0 0;
     0 0 0 1;
     0 0 0 -1]; % autonomous dynamics
-par.G = [0 0;
+mdl.G = [0 0;
         1 0;
         0 0
         0 1]; % control input
-par.theta_hat = [0; par.v0L1; 0; par.v0L2]; % constant dynamics
+mdl.theta_hat = [0; mdl.v0L1; 0; mdl.v0L2]; % constant dynamics
 
 % coordinate transformation %
 P_sub = [1 0;
@@ -45,24 +45,30 @@ P_sub = [1 0;
         0 1;
         1 0;
         0 1];
-par.P = blkdiag(P_sub, P_sub); % linear part
-omega_sub = [0; 0; -par.dh; 0; -2*par.dh; 0];
-par.omega = [omega_sub; omega_sub]; % constant part
+mdl.P = blkdiag(P_sub, P_sub); % linear part
+omega_sub = [0; 0; -mdl.dh; 0; -2*mdl.dh; 0];
+mdl.omega = [omega_sub; omega_sub]; % constant part
 
 % invariant manifold "error" parameters %
-par.D = par.A*par.P - par.P*par.F;
-par.E = par.A*par.omega + par.theta - par.P*par.theta_hat;
+mdl.D = mdl.A*mdl.P - mdl.P*mdl.F;
+mdl.E = mdl.A*mdl.omega + mdl.theta - mdl.P*mdl.theta_hat;
 
 % get ISS lyapunov matrices %
 % output we will use is velocity of each vehicle and intervehicle headways
-C_sub = [0 1 0 0 0 0;
-        1 0 -1 0 0 0;
-        0 0 0 1 0 0;
-        0 0 1 0 -1 0;
-        0 0 0 0 0 1];
-par.C = blkdiag(C_sub);
-M_bar = sdpvar(12,12);
-K_bar = sdpvar(12,12);
-F = [
+% C_sub = [0 1 0 0 0 0;
+%         1 0 -1 0 0 0;
+%         0 0 0 1 0 0;
+%         0 0 1 0 -1 0;
+%         0 0 0 0 0 1];
+% mdl.C = [C_sub zeros(5,6);
+%         0 0 0 0 1 0 -1 0 0 0 0 0;
+%         zeros(5,6) C_sub];
+M = sdpvar(12,12,'symmetric');
+K = sdpvar(2,12);
+F = [M >= 0];
+F = [F, (mdl.A + mdl.B*K)'*M + M*(mdl.A+mdl.B*K) <= 0];
+optimize(F, [], sdpsettings('solver','penlab'));
+mdl.M = value(M);
+mdl.K = value(K);
 
 end
