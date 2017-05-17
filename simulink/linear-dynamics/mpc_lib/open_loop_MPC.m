527933803c882449ec4eee1a28311edbb141e1f8
Stanley Smith <swsmth@berkeley.edu>
2017-05-17 09:49:18 -0700

    udpated decision variables in open-loop

--- simulink/linear-dynamics/mpc_lib/open_loop_MPC.m
+++ simulink/linear-dynamics/mpc_lib/open_loop_MPC.m
@@ -8,15 +8,26 @@ constraints = [];
 %%% SYSTEM_CONSTRAINTS %%%
 n = size(x0,1); % state dimension
 m = size(B,2); % input dimension
-u = sdpvar(m*T,1); % u = [u(t); u(t+1); ... u(t+T-1)] (both col vectors)
-x = sdpvar(n*T,1); % x = [x(t+1); x(t+2); ... x(t+T)] 
+x_bar = []; % x = [x(t+1); x(t+2); ... x(t+T)]
+u_bar = []; % u = [u(t); u(t+1); ... u(t+T-1)] (both col vectors)
+
+% create state decision variables for each time index
+for i = 1:T
+    x{i} = sdpvar(n,1);
+    u{i} = sdpvar(m,1);
+    x_bar = [x_bar; x{i}];
+    u_bar = [u_bar, u{i}];
+end
+
+% require that system updates satisfy x(t+1) = Ax(t) + Bu(t) + theta
 [G, M] = make_sys_constr(T, A, B, theta, x0);
-constraints = [constraints, x <= G*x + M];
-constraints = [constraints, x >= G*x + M];
+constraints = [constraints, x_bar <= G*u_bar + M];
+constraints = [constraints, x_bar >= G*u_bar + M];
 
 %%% INPUT_CONSTRAINTS %%%
+% require that inputs satisfy Hu*u(t) <= hu for all t
 [Hu_bar, hu_bar] = make_input_constr(T, Hu, hu);
-constraints = [constraints, Hu_bar*u <= hu_bar];
+constraints = [constraints, Hu_bar*u_bar <= hu_bar];
 
 %%% STL_CONSTRAINTS %%%
 % (a quantitative STL encoding is used - allows robustness of formula satisfaction)
@@ -71,6 +82,31 @@ end
 
 % define two variables as the conjunction of the predicates %
 % phi_t = mu_1(x_t) ^ mu_2(x_t)
-% psi_t = 
+% psi_t = mu_3(x_t) ^ mu_4(x_t) ^ mu_5(x_t) ^ mu_6(x_t)
+rt_phi = sdpvar(T,1);
+rt_psi = sdpvar(T,1);
+
+num_phi = 2;
+num_psi = 4; % number of conjunctions for each variable
+
+for i = 1:T
+    % create binary variables for conjunctions
+    pt_phi{i} = binvar(2,1);
+    pt_psi{i} = binvar(4,1);
+    % add conjunction constraints
+    constraints = [constraints, sum(pt_phi{i}) <= 1];
+    constraints = [constraints, sum(pt_phi{i}) >= 1];
+    constraints = [constraints, sum(pt_psi{i}) <= 1];
+    constraints = [constraints, sum(pt_psi{i}) >= 1];
+    for j = 1:num_phi
+    constraints = [constraints, rt_phi(i) <= rt_mu{j}(i)];
+    constraints = [constraints, rt_mu{j}(i) - (1 - pt_phi{i}(j))*M <= rt_phi(i)];
+    constraints = [constraints, rt_phi(i) <= 
+    end
+    for j = 1:num_psi
+    constraints = [constraints, rt_psi(i) <= rt_mu{2+j}(i)];
+    constraints = [constraints, 
+    end
+end
 
 end
