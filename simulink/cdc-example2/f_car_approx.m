d5aa23abd1dbf76c4f1e1091f00a252b7059abcd
Stanley Smith <swsmth@berkeley.edu>
2019-03-03 16:08:56 -0800

    updated files

--- simulink/cdc-example2/f_car_approx.m
+++ simulink/cdc-example2/f_car_approx.m
@@ -7,7 +7,7 @@ function f_approx = f_car_approx(x,u)
 %
 % Inputs are [v; omega]
 
-f_approx = [u(1)*(1 - 0.5*x(3)^2);
+f_approx = [u(1)*(1 - 0.47*x(3)^2);
             u(1)*(x(3) - 0.16*x(3)^3);
             u(2)];
 
